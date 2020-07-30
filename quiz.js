d3e4af539443f5d71b416442ffa2d4b1b6f585e5
ggstoltz <gunnar.b.stoltz@gmail.com>
2020-07-30 00:11:32 -0500

    Set Up

---
+++ quiz.js
@@ -0,0 +1,151 @@
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+
+// create our questions
+let questions = [
+  {
+    question: "Commonly used data types DO NOT include",
+    choiceA: "letters",
+    choiceB: "numbers",
+    choiceC: "vectors",
+    correct: "C",
+  },
+  {
+    question: "What is another way to list items to store?",
+    choiceA: "Array",
+    choiceB: "JQuery",
+    choiceC: "Bootstrap",
+    correct: "A",
+  },
+  {
+    question: "React is a product of what company?",
+    choiceA: "Nike",
+    choiceB: "Facebook",
+    choiceC: "Web Consortium",
+    correct: "B",
+  },
+];
+
+// create some variables
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
+
+// render a question
+function renderQuestion() {
+  let q = questions[runningQuestion];
+
+  question.innerHTML = "<p>" + q.question + "</p>";
+
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click", startQuiz);
+
+// start quiz
+function startQuiz() {
+  start.style.display = "none";
+  renderQuestion();
+  quiz.style.display = "block";
+  renderProgress();
+  renderCounter();
+  TIMER = setInterval(renderCounter, 1000);
+}
+
+// render progress
+function renderProgress() {
+  for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+  }
+}
+
+// counter render
+
+function renderCounter() {
+  if (count <= questionTime) {
+    counter.innerHTML = count;
+    timeGauge.style.width = count * gaugeUnit + "px";
+    count++;
+  } else {
+    count = 0;
+
+    answerIsWrong();
+    if (runningQuestion < lastQuestion) {
+      runningQuestion++;
+      renderQuestion();
+    } else {
+      clearInterval(TIMER);
+      scoreRender();
+    }
+  }
+}
+
+// check A
+
+function checkAnswer(answer) {
+  if (answer == questions[runningQuestion].correct) {
+    score++;
+
+    answerIsCorrect();
+  } else {
+    answerIsWrong();
+  }
+  count = 0;
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
+    clearInterval(TIMER);
+    scoreRender();
+  }
+}
+
+// answer is correct
+function answerIsCorrect() {
+  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong() {
+  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender() {
+  scoreDiv.style.display = "block";
+
+  // calculate the amount of question percent answered by the user
+  const scorePerCent = Math.round((100 * score) / questions.length);
+
+  let img =
+    scorePerCent >= 80
+      ? ""
+      : scorePerCent >= 60
+      ? ""
+      : scorePerCent >= 40
+      ? ""
+      : scorePerCent >= 20
+      ? ""
+      : "";
+
+  scoreDiv.innerHTML = "<img src=" + img + ">";
+  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+}
